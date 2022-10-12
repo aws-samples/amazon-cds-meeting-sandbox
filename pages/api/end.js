4e30111fabbc11970d6935081a5109b2313f471d
Court Schuett <schuettc@amazon.com>
2022-10-12 01:56:32 -0500

    fix: update envVar

--- pages/api/end.js
+++ pages/api/end.js
@@ -5,8 +5,8 @@ import {
 const config = {
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 };
 
